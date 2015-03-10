7421d2c74a91204f535e7e6561693fa4a3e7179b
angelcanto <angelcantovicente@gmail.com>
2015-03-10 18:12:44 +0100

    template upload

---
+++ PacMan - DFS/pacman-dfs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def dfs( r, c, pacman_r, pacman_c, food_r, food_c, grid):
+    return
+pacman_r, pacman_c = [ int(i) for i in raw_input().strip().split() ]
+food_r, food_c = [ int(i) for i in raw_input().strip().split() ]
+r,c = [ int(i) for i in raw_input().strip().split() ]
+
+grid = []
+for i in xrange(0, r):
+    grid.append(raw_input().strip())
+
+dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid)
